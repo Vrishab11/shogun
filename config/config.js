0b6cbadb9dedf6873f805d41764e3a1b37ead20c
Vrishab N <vrishabnalothayyil@gmail.com>
2024-09-06 15:02:47 +0530

    atls

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 
 const connectDB =()=>{
-  mongoose.connect("mongodb://127.0.0.1:27017/shogun")
+  mongoose.connect(process.env.MONGO_URL)
   .then(()=>{
   console.log("db connected");
 })
